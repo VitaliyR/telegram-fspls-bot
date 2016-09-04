e1c1680bc825df43b97b01abfd8d39df236edfa4
Vitaliy Ribachenko <vit@ribachenko.com>
2016-09-04 13:32:42 +0300

    Fix help message for group mode

--- controllers/help.js
+++ controllers/help.js
@@ -8,14 +8,24 @@ class HelpController extends Telegram.TelegramBaseController {
 	}
 
 	handle($) {
-		$.sendMessage(
-			'You can search and watch videos on FS via this bot.\n\n' +
-			'*Supported commands:*\n' +
+		const isGroup = $.message.chat.type === 'group';
+		$.sendMessage(isGroup ? this.getGroupMessage() : this.getPrivateMessage(), { parse_mode: 'Markdown' });
+	}
+
+	getPrivateMessage() {
+		return 'You can search and watch videos on FS via this bot.\n\n' +
+		'*Supported commands:*\n' +
+		'/help - show this message\n' +
+		'/h - show history of your watched movies and/or episodes\n' +
+		'/news - show latest bot news\n\n' +
+		'And you can, of course, just *type* movie name for *search*';
+	}
+
+	getGroupMessage() {
+		return 'You can search and watch videos on FS via this bot.\n\n' +
+			'*Supported commands in Group mode:*\n' +
 			'/help - show this message\n' +
-			'/h - show history of your watched movies and/or episodes\n' +
-			'/news - show latest bot news\n\n' +
-			'And you can, of course, just *type* movie name for *search*'
-			, { parse_mode: 'Markdown' });
+			'/news - show latest bot news';
 	}
 }
 
